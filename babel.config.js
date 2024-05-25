66159ea57d4fbfac022e59e86b858ce62bd39a2a
kokoball <kbp4154@naver.com>
2024-05-25 22:45:28 +0900

    절대 경로 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
