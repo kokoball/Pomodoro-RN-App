3d374b2a6efe470c644a5a9152e31cbfaa518deb
kokoball <kbp4154@naver.com>
2024-05-31 19:22:13 +0900

    metro.config 크러쉬 수정

--- metro.config.js
+++ metro.config.js
@@ -2,11 +2,7 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
-<<<<<<< HEAD
- * https://reactnative.dev/docs/metro
-=======
  * https://facebook.github.io/metro/docs/configuration
->>>>>>> d2661918b230cf21da1d34d2c0ffca424acd60fc
  *
  * @type {import('metro-config').MetroConfig}
  */
